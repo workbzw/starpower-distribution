0953b718c986bb5d3e3ac529483273c0babe44e4
bzw <workbzw@outlook.com>
2024-04-01 19:22:03 +0800

    [update] webhook_result

--- app/api/webhook_result/route.ts
+++ app/api/webhook_result/route.ts
@@ -4,18 +4,18 @@ import {createClient} from "@/utils/supabase/server";
 
 export async function GET() {
     // const supabase = createClient();
-    await axios.post("https://starpower-market.myshopify.com/admin/api/2024-01/webhooks.json", {
-        webhook: {
-            address: "https://starpower-distribution.vercel.app/api/webhook_order",
-            topic: "orders/paid",
-            format: "json"
-        }
-    }, {
-        headers: {
-            "X-Shopify-Access-Token": "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",
-            "Content-Type": "application/json",
-        }
-    })
+    // await axios.post("https://starpower-market.myshopify.com/admin/api/2024-01/webhooks.json", {
+    //     webhook: {
+    //         address: "https://starpower-distribution.vercel.app/api/webhook_order",
+    //         topic: "orders/paid",
+    //         format: "json"
+    //     }
+    // }, {
+    //     headers: {
+    //         "X-Shopify-Access-Token": "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",
+    //         "Content-Type": "application/json",
+    //     }
+    // })
     return Response.json({code: 200, msg: "", data: {}});
 }
 
